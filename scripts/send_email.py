75032e3b2632cd381d13c574587c302286445684
Arthur521 <arthurassis@hotmail.com.br>
2025-04-09 16:50:43 -0300

    Update send_email2

--- scripts/send_email.py
+++ scripts/send_email.py
@@ -6,15 +6,15 @@ def enviar_email(destinatario):
     msg = EmailMessage()
     msg.set_content("Pipeline executado com sucesso!")
     msg['Subject'] = "Notificação do Pipeline"
-    msg['From'] = "pipeline@githubactions.com"
+    msg['From'] = os.environ["SMTP_USER"]
     msg['To'] = destinatario
 
     try:
-        with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
+        with smtplib.SMTP("smtp.office365.com", 587) as smtp:
             smtp.starttls()
             smtp.login(os.environ["SMTP_USER"], os.environ["SMTP_PASS"])
             smtp.send_message(msg)
-        print("E-mail enviado com sucesso!")
+        print("E-mail enviado com sucesso via Outlook!")
     except Exception as e:
         print(f"Erro ao enviar e-mail: {e}")
 
